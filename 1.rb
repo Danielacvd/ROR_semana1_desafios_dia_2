7f19e7fbac2d076461f8584171c02f74f232d0b0
Daniel <dacevedosp@gmail.com>
2019-06-08 12:43:50 -0400

    Finish exercise one 1.rb

--- 1.rb
+++ 1.rb
@@ -1,5 +1,34 @@
 class Table
-    def initialize
-        
+    attr_accessor :mesa, :tablas
+    def initialize(mesa, *tablas)
+        @mesa = mesa
+        @tablas = *tablas.map(&:to_i)
+    end
+
+    def mayorxmesa
+        @tablas.max
+    end
+
+    def promedio
+        @tablas.inject(&:+)/@tablas.size.to_f
     end
 end
+
+file = File.open('casino.txt','r')
+data = file.readlines
+file.close
+puts data
+
+constablas = []
+data.each do |line|
+    #ls de line split
+    ls = line.split(', ')
+    constablas << Table.new(*ls)
+end
+
+constablas.each do |a|
+    puts "#{a.mayorxmesa} dia#{a.tablas.find_index(a.tablas.max)+1}"
+end
+
+promedioss = constablas.map(&:promedio)
+puts promedioss.inject(:+) / promedioss.size
